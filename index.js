a648d85a8fe2cabfc5d20b0908af843e855a21ca
Bernhard Hobel <bernhard.hobel@gmx.at>
2023-09-29 17:00:35 +0200

    exercise 3.5 create new person feature

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ let data = [
   }
 ]
 
+const generateId = () => {
+  return Math.floor(Math.random() * 1e6)
+}
+
 app.get('/api/persons', (request, response) => {
   response.json(data)
 })
@@ -49,6 +53,27 @@ app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   data = data.filter(person => person.id !== id)
   response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number missing'
+    })
+  }
+  if (data.map(person => person.name).includes(body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+  data = data.concat(person)
+  response.json(person)
 })
 
 const PORT = 3001
