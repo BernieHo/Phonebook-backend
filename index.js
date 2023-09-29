2f80318dd5d4c0f376e0a0a4b0ab00f734360832
Bernhard Hobel <bernhard.hobel@gmx.at>
2023-09-29 15:26:10 +0200

    exercise 3.3 add route for persons/id

--- index.js
+++ index.js
@@ -5,34 +5,44 @@ app.use(express.json())
 
 const data = [
   {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
+    'id': 1,
+    'name': 'Arto Hellas',
+    'number': '040-123456'
   },
   {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
+    'id': 2,
+    'name': 'Ada Lovelace',
+    'number': '39-44-5323523'
   },
   {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
+    'id': 3,
+    'name': 'Dan Abramov',
+    'number': '12-43-234345'
   },
   {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
+    'id': 4,
+    'name': 'Mary Poppendieck',
+    'number': '39-23-6423122'
   }
 ]
 
-app.get('/api/notes', (request, response) => {
+app.get('/api/persons', (request, response) => {
   response.json(data)
 })
 
 app.get('/info', (request, response) => {
   const now = new Date().toString()
   response.send(`<p>Phonebook has info for ${data.length} people</p><p>${now}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = data.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
 })
 
 const PORT = 3001
