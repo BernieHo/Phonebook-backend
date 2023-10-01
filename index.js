adb470e7308065d4406a0362df6224d6725246ed
Bernhard Hobel <bernhard.hobel@gmx.at>
2023-10-01 14:46:57 +0200

    exercise 3.8 add content logging

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const morgan = require('morgan')
+
+morgan.token('content', (req, res) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  }
+})
 const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 let data = [
   {
